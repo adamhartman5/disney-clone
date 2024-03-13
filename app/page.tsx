c6b2058faf18936d3c5421544cada67b45d38dfd
Adam Hartman <adamhartman5@gmail.com>
2024-03-13 01:51:12 -0400

    Continue building movie carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import MoviesCarousel from '@/components/MoviesCarousel';
+import {
+	getPopularMovies,
+	getTopRatedMovies,
+	getUpcomingMovies,
+} from '@/lib/getMovies';
 
-export default function Home() {
+export default async function Home() {
+	const upcomingMovies = await getUpcomingMovies();
+	const topRatedMovies = await getTopRatedMovies();
+	const popularMovies = await getPopularMovies();
+
 	return (
 		<main className=''>
-			<h1 className=''>Let's build Disney</h1>
-
 			{/* CarouselBannerWrapper */}
 
 			<div className='flex flex-col space-y-2 xl:mt-48'>
-				<MoviesCarousel movies={[]} title='upcoming' />
-				{/* <MoviesCarousel movies={...} title='upcoming' /> */}
-				{/* <MoviesCarousel movies={...} title='upcoming' /> */}
+				<MoviesCarousel movies={upcomingMovies} title='Upcoming' />
+				<MoviesCarousel movies={topRatedMovies} title='Top Rated' />
+				<MoviesCarousel movies={popularMovies} title='Popular' />
 			</div>
 		</main>
 	);
