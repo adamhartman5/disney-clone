5fd98781899b430e5d61b8313fd205fe0bf90df9
Adam Hartman <adamhartman5@gmail.com>
2024-03-14 01:31:57 -0400

    Finish building genre page

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,3 +1,4 @@
+import MoviesCarousel from '@/components/MoviesCarousel';
 import { getDiscoverMovies } from '@/lib/getMovies';
 import React from 'react';
 
@@ -14,8 +15,11 @@ async function GenrePage({ params: { id }, searchParams: { genre } }: Props) {
 	const movies = await getDiscoverMovies(id);
 
 	return (
-		<div>
-			Welcome to the genre with ID: {id} and name: {genre}
+		<div className='max-w-7xl mx-auto'>
+			<div className='flex flex-col space-y-5 mt-32 xl:mt-42'>
+				<h1 className='text-6xl font-bold px-10'>Results for {genre}</h1>
+				<MoviesCarousel title={`Genre`} movies={movies} isVertical />
+			</div>
 		</div>
 	);
 }
